03e083f16af0120d53286b4d9f6bde79af213960
Elviie <84927906+Massinja@users.noreply.github.com>
2023-03-25 11:58:55 -0400

    create my_cat.c

---
+++ my_cat.c
@@ -0,0 +1,46 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc, char **argv) 
+{
+    FILE *fp;
+    int i = 1;
+
+    while(i<argc)
+    {
+           
+        fp = fopen(argv[i], "r");
+        
+        if( fp == NULL)
+        {
+            fprintf(stderr, "fopen: Could not open '%s': %s", argv[i], strerror(errno));
+            fputs("\nCross fingers and continue", stderr);
+            break;
+        }
+
+        char c;
+
+        while((c = fgetc(fp)) != EOF)
+        {
+            printf("%c", c);
+        }
+
+        fclose(fp);
+        
+
+        i++;
+        if(i>argc)
+        {
+            printf("\n");
+        }
+
+    }
+
+    if(i==1)
+    {
+        fprintf(stderr, "no arguments found, enter file names!");
+    }
+ 
+    return 0;
+} 
